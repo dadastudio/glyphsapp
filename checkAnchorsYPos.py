f1b997beccf23f64a8314deb22e56b749a011164
Michal Jarocinski <michaljarocinski@Michals-MacBook-Pro.local>
2015-06-03 16:30:42 +0200

    Interface added
    Added floating window with controls

--- checkAnchorsYPos.py
+++ checkAnchorsYPos.py
@@ -3,36 +3,88 @@
 __doc__="""
 check anchors across the font
 """
-
 import GlyphsApp
+import vanilla
 Font = Glyphs.font
 
-
-selectedLayers = Font.selectedLayers
 Glyphs.clearLog()
 Glyphs.showMacroWindow()
 
-heights={"Light":460,"Regular":480,"Bold":514}
-currText=""
-master="Bold"
-anchor="top"
-Font.currentText=currText
-for gl in Font.glyphs:
+class AnchorsCheck(object):
+	leftMargin=15
+	lineHeight=30
 
-	if gl.subCategory=="Lowercase":
+	def __init__(self):
+		
 
-		for l in gl.layers:
+		self.w = vanilla.FloatingWindow( (300, 200), "Check anchors' positions", minSize=(300,200), maxSize=(1000,200) )#, autosaveName="pl.dadastudio.Test.mainwindow"
+		
+		self.w.text_1 = vanilla.TextBox((self.leftMargin, self.setLineHeight(0), -15, 14), "Anchor:", sizeStyle='small' )
+		
+		self.w.anchor_name = vanilla.PopUpButton((self.leftMargin+75, self.setLineHeight(0), -self.leftMargin, 17), self.GetAnchorNames(), sizeStyle='small' )
 
-			m=Font.masters[l.associatedMasterId]
+		self.w.text_3 = vanilla.TextBox((self.leftMargin, self.setLineHeight(1), -15, 14), "subCategory", sizeStyle='small' )
+		self.w.subCategory_name = vanilla.PopUpButton((self.leftMargin+75, self.setLineHeight(1), -self.leftMargin, 17), self.GetSubCategoryNames(), sizeStyle='small' )
 
-			if m.name == master:
-				if l.layerId == l.associatedMasterId: #only Master Layer
-					if l.anchors[anchor]: # if anchor exists
-						# print l
-						if l.anchors["top"].y != heights[m.name]:
-							currText=currText+" /"+gl.name
-							print gl.name+": " +str(l.anchors["top"].y)
+		self.w.text_4 = vanilla.TextBox((self.leftMargin, self.setLineHeight(2), -15, 14), "Expected Y", sizeStyle='small' )
+		self.w.setY = vanilla.EditText((self.leftMargin+75, self.setLineHeight(2), -self.leftMargin, 17), sizeStyle='small' )
+		
+		self.w.hLine_1 = vanilla.HorizontalLine((self.leftMargin, self.setLineHeight(3), -15, 1))
+		
+		self.w.moveButton = vanilla.Button((-80-self.leftMargin, -40, -self.leftMargin, -self.leftMargin), "Search", sizeStyle='regular', callback=self.MoveCallback )
+		self.w.setDefaultButton( self.w.moveButton )
+		
+		self.w.open()
+		self.w.center()
 
 
-Font.currentText=currText
-print currText
+	def setLineHeight(self,i):
+		return self.lineHeight*i+self.leftMargin
+
+	def MoveCallback( self, sender ):
+		master=Font.selectedFontMaster.name
+		currText=""
+		
+		height=int(self.w.setY.get())
+
+		subCategories=self.GetSubCategoryNames()
+		subCategory=subCategories[self.w.subCategory_name.get()]
+		
+		anchors=self.GetAnchorNames()
+		anchor=anchors[self.w.anchor_name.get()]
+
+		for gl in Font.glyphs:
+
+				if gl.subCategory==subCategory:
+
+					for l in gl.layers:
+
+						m=Font.masters[l.associatedMasterId]
+
+						if m.name == master:
+							if l.layerId == l.associatedMasterId: # only Master Layer
+								if l.anchors[anchor]: # if anchor exists
+									
+									anchorY=l.anchors[anchor].y
+									if anchorY != height: # and anchorY!=700:
+										currText=currText+"/"+gl.name+" "
+										print gl.name+": " +str(l.anchors[anchor].y)
+
+		if currText:
+
+			Font.currentText=currText
+			print currText
+		else :
+			print "ALL ANCHORS POSITIONED CORRECTLY!"
+								
+
+	def GetAnchorNames(self):
+		return ["top","bottom","center"]
+
+	def GetSubCategoryNames(self):
+		return ["Uppercase","Lowercase","Smallcaps"]
+
+
+
+AnchorsCheck()
+
